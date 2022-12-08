d484db9709da6b1ad10d5a484f674a88397269e8
Charles Swaleh <charlswall@live.com>
2022-12-08 11:59:37 +0300

    Add the beer name and beer image to the page when it loads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 // Code here
+const baseURL = "http://localhost:3000"
+
+
+// 1. Add the first beer's details in the HTMl when the page loads
+fetch(`${baseURL}/beers/1`)
+.then(res => res.json())
+.then(data => {
+  // Show the first beer on page load
+  const beerName = document.getElementById("beer-name");
+  beerName.textContent = data.name;
+  
+  const beerImage = document.getElementById("beer-image");
+  beerImage.setAttribute("src", data.image_url);
+  beerImage.setAttribute("alt", data.name);
+})
+.catch(err => console.log("Error: ", err))
