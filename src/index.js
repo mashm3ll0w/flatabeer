3c91edcfbbf1fa21f6fb1ff226f8d4b3d762d2f2
Charles Swaleh <charlswall@live.com>
2022-12-16 08:37:19 +0300

    Show the first beer details when the page loads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,33 @@
 // Code here
+
+baseURl = "http://localhost:3000/beers"
+
+// Get the first beer to show on the page
+function getBeer(id = 1){
+  fetch(`${baseURl}/${id}`)
+  .then(res => res.json())
+  .then(beer => {
+    populateBeerData(beer)
+  })
+  .catch(error => console.log("Error: ", error.message))
+}
+
+function populateBeerData(beer){
+  const beerDiv = document.querySelector(".beer-details")
+  beerDiv.querySelector("#beer-name").textContent = beer.name
+  beerDiv.querySelector("#beer-image").alt = beer.name
+  beerDiv.querySelector("#beer-image").src = beer.image_url
+  beerDiv.querySelector("#beer-description").textContent = beer.description
+  beerDiv.querySelector("#review-list").replaceChildren()
+  for (let review of beer.reviews){
+    const reviewList = beerDiv.querySelector("#review-list")
+    const li = document.createElement("li")
+    li.textContent = review
+    reviewList.appendChild(li)
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getBeer();
+})
+
